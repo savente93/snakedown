c410138a08723f55c6a38d6a5d35f4bb56f27730
Sam Vente <savente93@proton.me>
2026-01-27 23:41:10 +0100

    feat: suggest possible references on unknown ones

---
+++ tests/test_pkg/miss_spelled_ref.py
@@ -0,0 +1,11 @@
+"""this file is valid python but contains misspelled references for testing."""
+
+
+def the_little_function_that_could():
+    """kinda like [[test_pkg.bar.great]]
+
+    see also:
+        [[nimpy.fft]]
+        [[asdfasdfasdf]]
+    """
+    print("choo choo!")
